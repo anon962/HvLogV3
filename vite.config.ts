4adc8f5a00800ea61f99365720f8c1fa5d5d1c03
nope <nope>
2025-04-16 08:51:49 -0400

    .

--- vite.config.ts
+++ vite.config.ts
@@ -22,7 +22,7 @@ export default defineConfig((config) => {
                         "http://alt.hentaiverse.org/*",
                     ],
                     grant: ["unsafeWindow", "GM_addStyle"],
-                    version: "2.3",
+                    version: "2.4",
                     updateURL:
                         "https://github.com/anon962/HvLogV3/releases/download/latest/hvlog.user.js",
                     downloadURL:
